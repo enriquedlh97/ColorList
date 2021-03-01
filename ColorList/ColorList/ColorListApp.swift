0511d15dcd9bc738525658359684414a6fa531e2
enriquedlh97 <enriquedlh97@hotmail.com>
2021-02-28 17:59:48 -0800

    Created project

---
+++ ColorList/ColorList/ColorListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorListApp.swift
+//  ColorList
+//
+//  Created by user189351 on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ColorListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorList/ColorList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorList
+//
+//  Created by user189351 on 2/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
