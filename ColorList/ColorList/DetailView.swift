9f64bf6a654f3675f6942c71332b0480f1e25bd0
enriquedlh97 <enriquedlh97@hotmail.com>
2021-02-28 18:56:30 -0800

    Created MyColor struct

--- ColorList/ColorList/DetailView.swift
+++ ColorList/ColorList/DetailView.swift
@@ -2,7 +2,7 @@
 //  DetailView.swift
 //  ColorList
 //
-//  Created by user189351 on 2/28/21.
+//  Created by Enrique Diaz de Leon Hicks on 2/28/21.
 //
 
 import SwiftUI
---
+++ ColorList/ColorList/MyColor.swift
@@ -0,0 +1,16 @@
+//
+//  MyColor.swift
+//  ColorList
+//
+//  Created by Enrique Diaz de Leon Hicks on 2/28/21.
+//
+
+import SwiftUI
+
+struct MiColor: Identifiable {
+    
+    var id = UUID()
+    var text: String
+    var image: String
+    
+}
