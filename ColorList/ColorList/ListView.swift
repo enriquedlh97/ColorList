04da7e16b52f5486a2704dcdd09cb4aad94caa56
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-02 17:41:09 -0800

    Added dark and all colors filter

--- ColorList/ColorList/ListView.swift
+++ ColorList/ColorList/ListView.swift
@@ -10,62 +10,86 @@ import SwiftUI
 struct ListView: View {
     
     @StateObject var lists = MyColorModel()
+    @State var showDark: Bool = false
     
     var body: some View {
         NavigationView {
-            List {
-                Section(header:
-                            Text("American Palette"), content: {
-                                ForEach(lists.americanPalette) { item in
-                                    NavigationLink(
-                                        destination: DetailView(color: item),
-                                        label: {
-                                            VStack {
-                                                Text(item.nombre)
-                                                    .font(.largeTitle)
-                                                    .foregroundColor(item.colorTexto)
-                                                    .padding()
-                                            }
-                        
-                                        })
-                                        .listRowBackground(item.color)
-                                }
-                            })
-                Section(header:
-                            Text("Spanish Palette"), content: {
-                                ForEach(lists.spanishPalette) { item in
-                                    NavigationLink(
-                                        destination: DetailView(color: item),
-                                        label: {
-                                            VStack {
-                                                Text(item.nombre)
-                                                    .font(.largeTitle)
-                                                    .foregroundColor(item.colorTexto)
-                                                    .padding()
-                                            }
-                                            
-                                        })
-                                        .listRowBackground(item.color)
-                                }
-                            })
-                Section(header:
-                            Text("FlatUI Palette"), content: {
-                                ForEach(lists.flatUIPalette) { item in
-                                    NavigationLink(
-                                        destination: DetailView(color: item),
-                                        label: {
-                                            VStack {
-                                                Text(item.nombre)
-                                                    .font(.largeTitle)
-                                                    .foregroundColor(item.colorTexto)
-                                                    .padding()
-                                            }
-                                            
-                                        })
-                                        .listRowBackground(item.color)
-                                }
-                            })
-
+            VStack {
+                VStack {
+                    // Toggle
+                    Toggle(isOn: $showDark) {
+                        Text(showDark ? "All colors": "Dark colors" )
+                            .font(.title2)
+                            .foregroundColor(Color("DarkText"))
+                    }
+                    .padding(.top, 10)
+                    .padding(.horizontal, 80)
+                }
+                List {
+                    Section(header:
+                                Text("American Palette")
+                                .foregroundColor(Color("DarkText")), content: {
+                                    ForEach(lists.americanPalette) { item in
+                                        if showDark && item.modo == "Dark" || !showDark {
+                                            NavigationLink(
+                                                destination: DetailView(color: item),
+                                                label: {
+                                                    VStack {
+                                                        Text(item.nombre)
+                                                            .font(.largeTitle)
+                                                            .foregroundColor(item.colorTexto)
+                                                            .padding()
+                                                    }
+                                                    
+                                                })
+                                                
+                                                .listRowBackground(item.color)
+                                        }
+                                    }
+                                })
+                    Section(header:
+                                Text("Spanish Palette")
+                                .foregroundColor(Color("DarkText")), content: {
+                                    ForEach(lists.spanishPalette) { item in
+                                        if showDark && item.modo == "Dark" || !showDark {
+                                            NavigationLink(
+                                                destination: DetailView(color: item),
+                                                label: {
+                                                    VStack {
+                                                        Text(item.nombre)
+                                                            .font(.largeTitle)
+                                                            .foregroundColor(item.colorTexto)
+                                                            .padding()
+                                                    }
+                                                    
+                                                })
+                                                .listRowBackground(item.color)
+                                        }
+                                    }
+                                })
+                    Section(header:
+                                Text("FlatUI Palette")
+                                .foregroundColor(Color("DarkText")), content: {
+                                    ForEach(lists.flatUIPalette) { item in
+                                        if showDark && item.modo == "Dark" || !showDark {
+                                            NavigationLink(
+                                                destination: DetailView(color: item),
+                                                label: {
+                                                    VStack {
+                                                        Text(item.nombre)
+                                                            .font(.largeTitle)
+                                                            .foregroundColor(item.colorTexto)
+                                                            .padding()
+                                                    }
+                                                    
+                                                })
+                                                .listRowBackground(item.color)
+                                        }
+                                    }
+                                })
+                    
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationBarTitle("List")
             .navigationBarTitleDisplayMode(.inline)
